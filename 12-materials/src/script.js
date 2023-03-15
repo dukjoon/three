9dd928b6108480884b8cab9f67130913457f23db
Dukjoon Jung <ever@Dukjoonui-MacBookAir.local>
2023-03-15 23:35:29 +0900

    material gui add modified

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -92,9 +92,7 @@ const material = new THREE.MeshStandardMaterial();
 material.metalness = 0.7;
 material.roughness = 0.2;
 material.envMap = environmentMapTexture;
-material.
-
-gui.add(material, "metalness").min(0).max(1).step(0.0001);
+material.gui.add(material, "metalness").min(0).max(1).step(0.0001);
 gui.add(material, "roughness").min(0).max(1).step(0.0001);
 gui.add(material, "aoMapIntensity").min(0).max(1).step(0.0001);
 gui.add(material, "displacementScale").min(0).max(1).step(0.0001);
