68db2dd40fb224309d64e380a6e4a9a397a3a12f
Dukjoon Jung <ever@Dukjoonui-MacBookAir.local>
2023-04-07 17:14:51 +0900

    tomorrow end

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -4,6 +4,8 @@ import Time from "./Utils/Time";
 import Camera from "./Camera";
 import Renderer from "./Renderer";
 import World from "./World/World";
+import Resources from "./Utils/Resources";
+import sources from "./sources.js";
 
 let instance = null;
 
@@ -24,6 +26,7 @@ export default class Experience {
     // Setup
     this.sizes = new Sizes();
     this.time = new Time();
+    this.resources = new Resources(sources);
     this.scene = new THREE.Scene();
     this.camera = new Camera();
     this.renderer = new Renderer();
--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,7 +1,20 @@
+import * as THREE from "three";
 import Experience from "../experience";
+import Environment from "./Envorinment";
 
 export default class World {
   constructor() {
     this.experience = new Experience();
+    this.scene = this.experience.scene;
+
+    // Test Mesh
+    const testMesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial()
+    );
+    this.scene.add(testMesh);
+
+    // SunLight
+    const environment = new Environment();
   }
 }
